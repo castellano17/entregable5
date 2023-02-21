63fb60df657e1b94943b402f71ae6bb4b1a872a3
castellano17 <castellano17@gmail.com>
2023-02-21 14:32:55 -0600

    media query

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,11 +4,10 @@ import "./styles/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__red">
-        <h5 className="footer__by">By Esmir Roque Castellano</h5>
-      </div>
+      <div className="footer__red"></div>
 
       <div className="footer__black">
+        <h5 className="footer__by">By Esmir Roque Castellano</h5>
         <div className="footer__pokeball"></div>
       </div>
     </footer>
